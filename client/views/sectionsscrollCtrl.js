54ded43c51f91b7dafe65f95397fdb8b78b7a337
emilemuny <emilemuny007@gmail.com>
2015-05-14 06:57:22 -0700

    added parallax images

---
+++ client/views/sectionsscrollCtrl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('angular-prototype')
+  .controller('ScrollCtrl', ['$scope', '$document', function($scope, $document){
+    $scope.toTheTop = function() {
+      $document.scrollTopAnimated(0, 5000).then(function() {
+        console && console.log('You just scrolled to the top!');
+      });
+    };
+    var section3 = angular.element(document.getElementById('#section3'));
+    $scope.toSection3 = function() {
+      $document.scrollToElementAnimated(section3);
+    };
+
+  }
+]).value('duScrollOffset', 30);
